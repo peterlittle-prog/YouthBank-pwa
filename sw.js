dfe25629cefa41698af644f2341bf5a2918346c1
peterlittle-prog <peter.little@youthbankinternational.org>
2025-10-02 18:04:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,74 @@
-const CACHE_NAME = 'youthbank-sessions-v1';
-const API_URL = "https://script.google.com/a/macros/youthbankinternational.org/s/AKfycbw7yrHpVKHY3R2jX1QszH5eT6ixW6kQ5TmrR7pQCiT3_NA304KQIbz06R4oPq_I3aJn/exec";
-const urlsToCache = [ '/', '/index.html', '/style.css', '/app.js', API_URL ];
+const CACHE_NAME = 'youthbank-sessions-v2'; // Note: I incremented the version number
+const API_URL = "https://script.google.com/a/macros/youthbankinternational.org/s/AKfycbw7yrHpVKHY3R2jX1Qszh5eT6ixw6kQ5TmfR7QCiT3_NA304KQIBz06R40Pq_I3aJn/exec";
+const urlsToCache = [
+  '/',
+  '/YouthBank-pwa/', // Add this line for the root of the GitHub Pages site
+  '/YouthBank-pwa/index.html',
+  '/YouthBank-pwa/style.css',
+  '/YouthBank-pwa/app.js',
+  '/YouthBank-pwa/manifest.json',
+  API_URL
+];
 
 // Install the service worker and cache assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and caching assets');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 // Intercept fetch requests
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // Cache hit - return response from cache
-      if (response) {
-        return response;
-      }
-      // Not in cache - fetch from network, then cache it
-      return fetch(event.request).then(networkResponse => {
-        // ... logic to cache the new response ...
-        return networkResponse;
-      });
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response from cache
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network, then cache it for next time
+        return fetch(event.request).then(
+          networkResponse => {
+            // Check if we received a valid response
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic' && networkResponse.type !== 'cors') {
+              return networkResponse;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need to clone it
+            // so we have two streams.
+            const responseToCache = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
