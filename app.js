5d8a6167a83b59b518c19cee7126cb096c29cce7
peterlittle-prog <peter.little@youthbankinternational.org>
2025-10-04 13:23:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const script = document.createElement('script');
   
   // IMPORTANT: Make sure this is your latest, active Apps Script deployment URL
-  script.src = "YOUR_LATEST_APPS_SCRIPT_URL?callback=displaySessions";
+  script.src = "https://script.google.com/a/macros/youthbankinternational.org/s/AKfycbw7yrHpVKHY3R2jX1QszH5eT6ixW6kQ5TmrR7pQCiT3_NA304KQIbz06R4oPq_I3aJn/exec?callback=displaySessions";
   
   document.body.appendChild(script);
 
